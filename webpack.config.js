ea4c4fff5c08a571a025838e1ab4791e0e826847
Qusic <hi@qusic.me>
2016-12-02 10:57:05 +0800

    remove unnecessary query

--- webpack.config.js
+++ webpack.config.js
@@ -79,7 +79,7 @@ if (production) {
   config.entry.unshift(
     'react-hot-loader/patch',
     'webpack/hot/only-dev-server',
-    `webpack-dev-server/client?http://${devHost}:${devPort}`
+    'webpack-dev-server/client'
   )
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
