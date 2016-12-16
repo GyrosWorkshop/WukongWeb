25666c44783e9d3da4f7b201b16b3782675127a7
Qusic <hi@qusic.me>
2016-12-17 01:51:25 +0800

    first time join channel

--- src/component/welcome/index.js
+++ src/component/welcome/index.js
@@ -1,19 +1,43 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router'
 import CSSModules from 'react-css-modules'
 
+import Action from '../../action'
 import style from './style.sss'
 
+function mapStateToProps(state) {
+  return {
+    channel: state.channel.name
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    joinChannel(channel) {
+      dispatch(Action.Channel.name.create(channel))
+    }
+  }
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 @CSSModules(style)
 export default class Welcome extends Component {
+  static propTypes = {
+    channel: PropTypes.string,
+    joinChannel: PropTypes.func
+  }
+
   state = {
     channel: ''
   }
 
-  onChannelChange = (event) => {
+  onChannelInput = (event) => {
     this.setState({channel: event.target.value})
   }
 
   onStartAction = (event) => {
+    this.props.joinChannel(this.state.channel)
   }
 
   render() {
@@ -21,11 +45,13 @@ export default class Welcome extends Component {
       <div styleName='container'>
         <input styleName='channel-field'
           value={this.state.channel}
-          onChange={this.onChannelChange}/>
+          onChange={this.onChannelInput}/>
         <a styleName='start-button'
           onTouchTap={this.onStartAction}>
           <i className='fa fa-play'/> Start
         </a>
+        {this.props.channel &&
+          <Redirect to={{pathname: `/${this.props.channel}`}}/>}
       </div>
     )
   }
