97b0d2f55f4d8f34810040a303c3b03408bafdaa
Qusic <hi@qusic.me>
2016-12-17 02:16:16 +0800

    test code

--- src/component/channel/index.js
+++ src/component/channel/index.js
@@ -1,13 +1,45 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router'
 import CSSModules from 'react-css-modules'
 
+import Action from '../../action'
 import style from './style.sss'
 
+function mapStateToProps(state) {
+  return {
+    channel: state.channel.name
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    exitChannel() {
+      dispatch(Action.Channel.name.create(''))
+    }
+  }
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 @CSSModules(style)
 export default class Channel extends Component {
+  static propTypes = {
+    channel: PropTypes.string,
+    exitChannel: PropTypes.func
+  }
+
+  onExitAction = (event) => {
+    this.props.exitChannel()
+  }
+
   render() {
     return (
-      <div>Channel</div>
+      <div>
+        <p>Channel: {this.props.channel}</p>
+        <a href='#' onTouchTap={this.onExitAction}>Exit</a>
+        {!this.props.channel &&
+          <Redirect to={{pathname: '/'}}/>}
+      </div>
     )
   }
 }
