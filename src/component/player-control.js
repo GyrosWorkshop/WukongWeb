cf142b5a933ae5ad34907d35c5ad0be52893266e
Qusic <hi@qusic.me>
2017-01-05 01:44:04 +0800

    fix lint

--- src/component/player-control.js
+++ src/component/player-control.js
@@ -1,15 +1,33 @@
 import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
 import CSSModules from 'react-css-modules'
 
 import style from './player-control.sss'
 
+function mapStateToProps(state) {
+  return {
+    playing: state.song.playing.file,
+    preload: state.song.preload.file,
+    connection: state.user.preferences.connection
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {}
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 @CSSModules(style)
 export default class PlayerControl extends Component {
   static propTypes = {
-    file: PropTypes.string
+    playing: PropTypes.object,
+    preload: PropTypes.object,
+    connection: PropTypes.number
   }
 
   render() {
-    return
+    return (
+      <div styleName='container'></div>
+    )
   }
 }
