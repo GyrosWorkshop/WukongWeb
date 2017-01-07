32483636b33d93b151f8b55991374726348a3e2f
Qusic <hi@qusic.me>
2017-01-08 02:01:00 +0800

    player control audio

--- src/component/player-control.js
+++ src/component/player-control.js
@@ -1,34 +1,16 @@
-import React, {PureComponent, PropTypes} from 'react'
-import {connect} from 'react-redux'
+import React, {PureComponent} from 'react'
 import CSSModules from 'react-css-modules'
 
-import Selector from '../selector'
+import PlayerControlAudio from './player-control-audio'
 import style from './player-control.sss'
 
-function mapStateToProps(state) {
-  return {
-    player: state.player,
-    playing: Selector.playingFile(state),
-    preload: Selector.preloadFile(state)
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {}
-}
-
-@connect(mapStateToProps, mapDispatchToProps)
 @CSSModules(style)
 export default class PlayerControl extends PureComponent {
-  static propTypes = {
-    player: PropTypes.object,
-    playing: PropTypes.object,
-    preload: PropTypes.object
-  }
-
   render() {
     return (
-      <div styleName='container'></div>
+      <div styleName='container'>
+        <PlayerControlAudio/>
+      </div>
     )
   }
 }
