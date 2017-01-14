537b27b30c24926acdf6971e671566f022ece0b4
Qusic <hi@qusic.me>
2017-01-14 14:48:32 +0800

    volume slider

--- src/component/volume-slider.js
+++ src/component/volume-slider.js
@@ -27,12 +27,31 @@ export default class VolumeSlider extends PureComponent {
     dispatchVolume: PropTypes.func
   }
 
+  onInputChange = (event) => {
+    const input = parseFloat(event.target.value)
+    this.props.dispatchVolume(input)
+  }
+
+  onMinAction = (event) => {
+    this.props.dispatchVolume(0)
+  }
+
+  onMaxAction = (event) => {
+    this.props.dispatchVolume(1)
+  }
+
   render() {
+    const {volume} = this.props
     return (
       <div styleName='container'>
-        <i className='fa fa-volume-down'/>
-        <input type='range'/>
-        <i className='fa fa-volume-up'/>
+        <button onTouchTap={this.onMinAction}>
+          <i className='fa fa-volume-down'/>
+        </button>
+        <input type='range' min={0} max={1} step={0.01}
+          value={volume} onChange={this.onInputChange}/>
+        <button onTouchTap={this.onMaxAction}>
+          <i className='fa fa-volume-up'/>
+        </button>
       </div>
     )
   }
