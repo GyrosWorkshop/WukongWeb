609696b905f09085a02f83fbae042e5e93bcff99
Qusic <hi@qusic.me>
2017-03-27 22:13:39 +0800

    scroll body

--- src/component/app.js
+++ src/component/app.js
@@ -29,7 +29,7 @@ export default class App extends PureComponent {
     return (
       <DocumentTitle title='Wukong'>
         <BrowserRouter>
-          <div className='app'>
+          <div>
             <Route path='/' exact component={
               lazy(() => import('./welcome'))
             }/>
