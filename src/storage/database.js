9ff68fee1f667d2d1aec8ea5530257fe3f7f84c9
Qusic <hi@qusic.me>
2017-04-30 11:27:26 +0800

    optimize storage

--- src/storage/database.js
+++ src/storage/database.js
@@ -1,21 +1,25 @@
-import {merge, pick} from 'lodash'
+import {merge, pick, isEqual} from 'lodash'
 
 const key = 'wukong'
+const data = {
+  open: {
+    user: {
+      auth: {
+        status: true
+      },
+      preferences: {
+        listenOnly: false,
+        connection: 0,
+        audioQuality: 2
+      }
+    }
+  },
+  save: {}
+}
 
 export function open(state = {}) {
   try {
-    return merge({
-      user: {
-        auth: {
-          status: true
-        },
-        preferences: {
-          listenOnly: false,
-          connection: 0,
-          audioQuality: 2
-        }
-      }
-    }, JSON.parse(localStorage.getItem(key)), state)
+    return merge(data.open, JSON.parse(localStorage.getItem(key)), state)
   } catch (error) {
     return state
   }
@@ -23,11 +27,14 @@ export function open(state = {}) {
 
 export function save(state = {}) {
   try {
-    localStorage.setItem(key, JSON.stringify(pick(state, [
+    const data = pick(state, [
       'user.preferences',
       'song.playlist',
       'player.volume'
-    ])))
+    ])
+    if (isEqual(data, data.save)) return
+    localStorage.setItem(key, JSON.stringify(data))
+    data.save = data
   } catch (error) {
     localStorage.removeItem(key)
   }
