bedcb053906dfd97ee971d3f51999d0e0060fa70
Qusic <qusicx@gmail.com>
2018-05-11 11:25:32 +0800

    migrate babel and postcss config to js

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,30 @@
 module.exports = function(api) {
-  return {
+  api.cache(true)
+  const config = {
     presets: [
       ['@babel/preset-env', {
-        modules: false, // test: 'commonjs'
+        modules: false,
         useBuiltIns: 'usage'
-        // test targets: {node: 'current'}
       }],
       ['@babel/preset-stage-2', {
         decoratorsLegacy: true
       }],
       ['@babel/preset-react']
     ],
-    plugins: [
-      'react-hot-loader/babel'
-    ]
+    plugins: []
   }
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      config.plugins.push('react-hot-loader/babel')
+      break
+    case 'production':
+      break
+    case 'test':
+      config.presets[0][1].modules = 'commonjs'
+      config.presets[0][1].targets = {
+        node: 'current'
+      }
+      break
+  }
+  return config
 }
