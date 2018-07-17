f56d78aafe53cd56fca3fb6e7f0a3f47d3ff757e
Qusic <qusicx@gmail.com>
2018-07-17 15:34:40 +0800

    speech supports more browsers

--- src/enhancer/speech.js
+++ src/enhancer/speech.js
@@ -17,48 +17,29 @@ export default applyMiddleware(
               !prevMembers.find(prevMember => member.id == prevMember.id)
             ).map(member => member.nickname)
             if (nicknames.length) {
-              say({
-                'zh-CN': [
-                  '欢迎我们的老伙计，',
-                  `${nicknames.join('和')}来到悟空${name}房间。`
-                ].join(''),
-                'en-US': [
-                  'huan ying wo men de lao hoh ji,',
-                  `${nicknames.join(' hur ')} lie dao`,
-                  `wu kong ${name} fang jian.`
-                ].join(' ')
-              })
+              say([
+                'huan ying wo men de lao hoh ji,',
+                `${nicknames.join(' hur ')} lie dao`,
+                `wu kong ${name} fang jian.`
+              ].join(' '))
             }
           } else {
             const nicknames = prevMembers.filter(prevMember =>
               !members.find(member => member.id == prevMember.id)
             ).map(member => member.nickname)
             if (nicknames.length) {
-              say({
-                'zh-CN': [
-                  `噢，${nicknames.join('和')}离开了悟空，`,
-                  '我向上帝保证，',
-                  '这简直是糟透了。'
-                ].join(''),
-                'en-US': [
-                  `oh, ${nicknames.join(' hur ')} lee kai lur wu kong,`,
-                  'wo xiang shang dee bow zheng,',
-                  'zhe jian zhi shee zao toe leh.'
-                ].join(' ')
-              })
+              say([
+                `oh, ${nicknames.join(' hur ')} lee kai lur wu kong,`,
+                'wo xiang shang dee bow zheng,',
+                'zhe jian zhi shee zao toe leh.'
+              ].join(' '))
             }
           }
         } else {
-          say({
-            'zh-CN': [
-              `嘿，${profile.nickname}，`,
-              '见到你真是太令人高兴了。'
-            ].join(''),
-            'en-US': [
-              `hey, ${profile.nickname},`,
-              'jian dao ni zhen shee tie ling reng gao xing leh.'
-            ].join(' ')
-          })
+          say([
+            `hey, ${profile.nickname},`,
+            'jian dao ni zhen shee tie ling reng gao xing leh.'
+          ].join(' '))
         }
         break
       }
@@ -71,23 +52,24 @@ const say = (() => {
     let voice
     const selectVoice = () => {
       const keySorter = (key, values) => (object) =>
-        values.concat('').findIndex(value => object[key].includes(value))
-      const langSort = keySorter('lang', ['zh-CN', 'en-US'])
-      const uriSort = keySorter('voiceURI', ['Google', 'Microsoft'])
+        values.concat('').findIndex(value =>
+          object[key].toLowerCase().includes(value.toLowerCase()))
+      const sorters = [
+        keySorter('lang', ['en-US', 'en']),
+        keySorter('voiceURI', ['Google', 'Microsoft', 'Alex'])
+      ]
       voice = speechSynthesis.getVoices().sort((voice1, voice2) => {
-        const lang1 = langSort(voice1), lang2 = langSort(voice2)
-        const uri1 = uriSort(voice1), uri2 = uriSort(voice2)
-        if (lang1 != lang2) return lang1 - lang2
-        if (uri1 != uri2) return uri1 - uri2
+        for (const sorter of sorters) {
+          const result = sorter(voice1) - sorter(voice2)
+          if (result != 0) return result
+        }
         return 0
       })[0]
     }
     selectVoice()
     speechSynthesis.onvoiceschanged = selectVoice
-    return (texts) => {
-      if (!voice) return
-      const text = texts[voice.lang]
-      if (!text) return
+    return (text) => {
+      if (!voice || !text) return
       const utterance = new SpeechSynthesisUtterance()
       utterance.voice = voice
       utterance.text = text
